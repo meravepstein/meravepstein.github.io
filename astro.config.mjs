ef39d775b383af9d6cb2642a727b4686074efcf1
itaie <itainepstein@gmail.com>
2025-02-09 17:26:01 -0500

    correct site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "www.animerav.me",
+    site: "https://www.animerav.me",
     integrations: [tailwind()],
 });
